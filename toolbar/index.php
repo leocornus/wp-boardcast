cbaaba87afaa2ccc233d624c3aa6e4603540a5f6
Sean Chen <sean.chen@leocorn.com>
2017-02-24 10:08:22 -0500

    add the boardcast toolbar on WordPress admin bar.

---
+++ toolbar/index.php
@@ -0,0 +1,32 @@
+<?php
+// if this file is called directly, abort!
+if(!defined('WPINC')) {
+    die;
+}
+
+/**
+ * register and enqueue boardcast toolbar styles.
+ */
+wp_register_style('boardcast-toolbar', 
+                  plugins_url('boardcast-toolbar.css', __FILE__));
+wp_enqueue_style('boardcast-toolbar');
+
+/**
+ * add the boardcast menu on the wordpress toolbar.
+ */
+function add_boardcast_toolbar_menu() {
+
+    global $wp_admin_bar;
+    $icon = '<span class="ab-icon"></span>';
+
+    // add Projects menu for all users.
+    $wp_admin_bar->add_menu( array(
+        'id' => 'boardcast',
+        'parent' => 'top-secondary',
+        //'title' => $icon . '<span id="ab-pending-notifications" class="count alert">0</span>'
+        'title' => $icon . 'Site news'
+    ));
+}
+
+// hook to action 'admin_bar_menu'.
+add_action('admin_bar_menu', 'add_boardcast_toolbar_menu',25);
