e7d819184225d905bc7103aae29f8d81ecdfd26c
Sean Chen <sean.chen@leocorn.com>
2017-03-01 08:34:02 -0500

    play around the position (offset) and animation strategy

--- toolbar/boardcast-toolbar.js
+++ toolbar/boardcast-toolbar.js
@@ -4,20 +4,24 @@ jQuery(document).ready(function($) {
     var tooltip = '<span class="tip">Check our new updates</span>';
     $('body').append(tooltip);
 
+    // find the right position:
+    var left = $('#wp-admin-bar-boardcast > .ab-item > .ab-icon').offset()['left'];
+
     //var selector = "div.tooltips > span";
     var selector = "span.tip";
 
     var showProperty = {
-      opacity: 1,
-      top: "43px",
-      left: "900px",
-      "margin-left": "-76px",
+      "opacity": 1,
+      "top": "43px",
+      "left": left + "px",
+      "margin-left": "-180px",
     };
-    $(selector).animate(showProperty, 'slow',
+
+    $(selector).animate(showProperty, 2000,
                                      function() {
         setTimeout(function() {
             // set the tooltips to disappear
-            $(selector).animate({opacity: 0}, 'slow');
-        }, 2000);
+            $(selector).animate({opacity: 0}, 1000);
+        }, 3000);
     });
 });
